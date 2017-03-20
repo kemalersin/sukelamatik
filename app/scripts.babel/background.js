8b6936bf2a7021f27e57aaaf5831d53d48ee307d
Kemal Ersin YILMAZ <mail@kemalersin.com>
2017-03-20 13:39:34 +0300

    Yenile düğmesi eklendi.

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -1,37 +1,51 @@
 'use strict';
 
-const url = 'https://eksisozluk.com';
+(function () {
+  const url = 'https://eksisozluk.com';
 
-chrome.runtime.onInstalled.addListener(details => {
-  chrome.alarms.create('update', {periodInMinutes: 0.1});
-});
+  function getEntry() {
+    $.get(`${url}/basliklar/gundem`)
+      .done(topicData => {
+        let topics = $(topicData).find('ul.topic-list.partial > li > a').toArray();
+        let topic = topics[Math.floor(Math.random() * topics.length)];
 
-chrome.alarms.onAlarm.addListener(alarm => {
-  $.get(`${url}/basliklar/gundem`)
-    .done(topicData => {
-      let topics = $(topicData).find('ul.topic-list.partial > li > a').toArray();
-      let topic = topics[Math.floor(Math.random() * topics.length)];
+        $.get(`${url}${topic.pathname}?a=dailynice`)
+          .done(entryData => {
+            let title = $(entryData).find('h1#title')[0];
+            let entries = $(entryData).find('ul#entry-list > li').toArray();
+            let entry = entries[Math.floor(Math.random() * entries.length)];
 
-      $.get(`${url}${topic.pathname}?a=dailynice`)
-        .done(entryData => {
-          let title = $(entryData).find('h1#title')[0];
-          let entries = $(entryData).find('ul#entry-list > li').toArray();
-          let entry = entries[Math.floor(Math.random() * entries.length)];
+            let entryObject = {
+              id: $(entry).data('id'),
+              author: $(entry).data('author'),
+              authorId: $(entry).data('author-id'),
+              favoriteCount: $(entry).data('favorite-count'),
+              date: $(entry).find('footer > .info > a.entry-date').text(),
 
-          let entryObject = {
-            id: $(entry).data('id'),
-            author: $(entry).data('author'),
-            authorId: $(entry).data('author-id'),
-            favoriteCount: $(entry).data('favorite-count'),
-            date: $(entry).find('footer > .info > a.entry-date').text(),
+              titleHTML: title.outerHTML,
+              entryHTML: entry.outerHTML
+            };
 
-            titleHTML: title.outerHTML,
-            entryHTML: entry.outerHTML
-          };
+            chrome.storage.local.set({'entry': entryObject});
 
-          chrome.storage.local.set({'entry': entryObject});
-          chrome.browserAction.setBadgeText({text: 'Yeni!'});
-        })
-    })
-    .fail(console.log);
-});
+            chrome.browserAction.setBadgeText({text: 'Yeni!'});
+            chrome.browserAction.setBadgeBackgroundColor({color: '#388e3c'});
+            chrome.browserAction.enable();
+
+            chrome.runtime.sendMessage({update: true});
+          })
+      })
+      .fail(err => {
+        console.log(err);
+        chrome.browserAction.disable();
+      });
+  }
+
+  chrome.runtime.onInstalled.addListener(details => {
+    getEntry();
+
+    chrome.browserAction.disable();
+    chrome.alarms.create('update', {periodInMinutes: 0.1});
+    chrome.alarms.onAlarm.addListener(alarm => getEntry());
+  });
+}());
--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -1,26 +1,56 @@
 'use strict';
 
-chrome.browserAction.setBadgeText({text: ''});
-
-chrome.storage.local.get('entry', result => {
-  let entry = result.entry;
-
-  $('#entry-list')
-    .before(entry.titleHTML)
-    .append(entry.entryHTML);
-
-  $('body').on('click', 'a', (event) => {
-    let el = event.currentTarget;
-
-    if ($(el).hasClass('url')) {
-      return;
-    }
-
-    event.preventDefault();
-    event.stopPropagation();
-
+(function () {
+  function showOptions() {
     chrome.tabs.create({
-      url: `https://eksisozluk.com${el.pathname}${el.search}`
+      url: `chrome://extensions/?options=${chrome.runtime.id}`
     });
+  }
+
+  function showEntry() {
+    chrome.browserAction.setBadgeText({text: ''});
+
+    chrome.storage.local.get('entry', result => {
+      let entry = result.entry;
+
+      $('button.refresh').hide();
+
+      if (!entry) {
+        return;
+      }
+
+      $('#title').remove();
+
+      $('#entry-list')
+        .html('')
+        .before(entry.titleHTML)
+        .append(entry.entryHTML);
+    });
+  }
+
+  $('body')
+    .on('click', 'button.refresh', showEntry)
+    .on('click', 'button.options', showOptions)
+    .on('click', 'a', (event) => {
+      let el = event.currentTarget;
+
+      if ($(el).hasClass('url')) {
+        return;
+      }
+
+      event.preventDefault();
+      event.stopPropagation();
+
+      chrome.tabs.create({
+        url: `https://eksisozluk.com${el.pathname}${el.search}`
+      });
+    });
+
+  chrome.runtime.onMessage.addListener((msg, sender) => {
+    if (msg.update) {
+      $('button.refresh').show();
+    }
   });
-});
+
+  showEntry();
+}());
